9e9f7e04c6201f9c5111b6d0559de33a8a445e0b
jzq23adu <jzq23adu@uea.ac.uk>
2025-11-10 18:58:48 +0000

    Revert "Update Home.jsx"
    This reverts commit ab66bf99d7a3666fa106533e2988ab08a324d7c3.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,24 @@ import WeatherCard from "../components/WeatherCard";
 import "../styles/Home.css"
 import Navbar from "../components/Navbar";
 
+// function Home(){
+//     return(
+//         <div className="home">
+//             <div className="home-content">
+//                 <h1> Your Weather Companion </h1>
+//                 <WeatherCard/>
+//                 <WeatherCard/>
+//                 <WeatherCard/>
+//                 <p className="description">
+//                     Daily accurate forecast tailored for you
+//                 </p>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Home;
+
 function Home(){
     const [city, setCity] = useState("London");
     const [data, setData] = useState(null);
