6ba415e0d3631405c0bc74df63557a8965107c8c
lzam <110193626+lzam0@users.noreply.github.com>
2026-01-19 18:26:38 +0000

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist',   // Keep build inside frontend folder
   },
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000'
-      }
-    },
   },
+  base: '/'           // ensures correct routing for React Router
 })
